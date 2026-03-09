4d9648329616eaee8163188f2ef06b166faaf696
Juliana Amparo Cabrera <jaacabrera.school@gmail.com>
2026-03-09 22:17:41 +0800

    Delete Archive directory

--- Archive/duplicates.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_excel("HR_Analysis_Output.xlsx", sheet_name=None)
-
-# Show all sheet names
-print(df.keys())
-
-# Show columns for the Promotion & Transfer sheet
-print(df["Promotion & Transfer"].columns)
